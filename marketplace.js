095235cfc0ab09210d537487739ba18142b081a6
Daniel Huigens <d.huigens@gmail.com>
2014-11-09 22:35:19 +0100

    Formatting

--- marketplace.js
+++ marketplace.js
@@ -2,51 +2,49 @@ var apps = document.getElementById('apps');
 var search = document.getElementById('search');
 
 function searchApps() {
-
-var req = new XMLHttpRequest();
-req.onreadystatechange = function() {
-	if(req.readyState === 4 && req.status === 200) {
-		var resp = JSON.parse(req.responseText);
-		apps.innerHTML = '';
-		resp.objects.forEach(function(object) {
-			var app = document.createElement('div');
-			app.className = 'app';
-			var icon = document.createElement('img');
-			icon.src = object.icons[64];
-			app.appendChild(icon);
-			var name = document.createElement('div');
-			name.className = 'name';
-			name.appendChild(document.createTextNode(object.name[object.default_locale]));
-			app.appendChild(name);
-			var description = document.createElement('div');
-			description.className = 'description';
-			description.appendChild(document.createTextNode(object.description[object.default_locale]));
-			app.appendChild(description);
-			var installLink = document.createElement('a');
-			installLink.className = 'installLink';
-			installLink.href = '#';
-			installLink.addEventListener('click', function(evt) {
-				evt.preventDefault();
-				installLink.textContent = 'Installing…';
-				var request = navigator.mozApps.installPackage(object.manifest_url, {catagories: object.categories});
-				request.onsuccess = function() {
-					console.log(request);
-					installLink.textContent = 'Installed';
-				};
-				request.onerror = function() {
-					console.log(request);
-					installLink.textContent = 'There was an error. Try again';
-				};
+	var req = new XMLHttpRequest();
+	req.onreadystatechange = function() {
+		if(req.readyState === 4 && req.status === 200) {
+			var resp = JSON.parse(req.responseText);
+			apps.innerHTML = '';
+			resp.objects.forEach(function(object) {
+				var app = document.createElement('div');
+				app.className = 'app';
+				var icon = document.createElement('img');
+				icon.src = object.icons[64];
+				app.appendChild(icon);
+				var name = document.createElement('div');
+				name.className = 'name';
+				name.appendChild(document.createTextNode(object.name[object.default_locale]));
+				app.appendChild(name);
+				var description = document.createElement('div');
+				description.className = 'description';
+				description.appendChild(document.createTextNode(object.description[object.default_locale]));
+				app.appendChild(description);
+				var installLink = document.createElement('a');
+				installLink.className = 'installLink';
+				installLink.href = '#';
+				installLink.addEventListener('click', function(evt) {
+					evt.preventDefault();
+					installLink.textContent = 'Installing…';
+					var request = navigator.mozApps.installPackage(object.manifest_url, {catagories: object.categories});
+					request.onsuccess = function() {
+						console.log(request);
+						installLink.textContent = 'Installed';
+					};
+					request.onerror = function() {
+						console.log(request);
+						installLink.textContent = 'There was an error. Try again';
+					};
+				});
+				installLink.textContent = 'Install';
+				app.appendChild(installLink);
+				apps.appendChild(app);
 			});
-			installLink.textContent = 'Install';
-			app.appendChild(installLink);
-			apps.appendChild(app);
-		});
-	}
-};
-req.open('GET', 'http://marketplace-dev.airborn.io/api/v1/apps/search/?q=' + encodeURIComponent(search.value) + '&device=desktop&app_type=packaged');
-req.send();
-
+		}
+	};
+	req.open('GET', 'http://marketplace-dev.airborn.io/api/v1/apps/search/?q=' + encodeURIComponent(search.value) + '&device=desktop&app_type=packaged');
+	req.send();
 }
 
 searchApps();
